2331d93c2799a45a9faf2a2f8fddfa1f4f885ada
l0rb <lorbritzer@yahoo.de>
2018-12-23 22:40:24 +0100

    made it through a midi file, finding all events

--- read_midi.py
+++ read_midi.py
@@ -1,4 +1,5 @@
 import struct
+import io
 
 CHUNK_TYPE_HEADER = b'MThd'
 CHUNK_TYPE_TRACK = b'MTrk'
@@ -29,6 +30,7 @@ def read_header(f):
     form = read16bit(f)
     tracks = read16bit(f)
     division = f.read(2)
+    print('TRACKS', tracks)
     return form, tracks, division
 
 def read_meta(f):
@@ -61,9 +63,82 @@ def read_sysex(f):
     print('SKIP SYSEX BYTES', r, se.hex())
     return r
 
+def reset_all_controllers():
+    print('RESET ALL CONTROLLERS')
+
+def read_controller_change(f, channel):
+    data = f.read(2)
+    bytes_read = 2
+    print('CHANNEL', channel, 'data', hex(data[0]))
+    if data[0] < 0x78 or data[0] > 0x7f:
+        print('CHANNEL VOICE MESSAGE on CONTROLLER', hex(data[0]), hex(data[1]))
+    else:
+        if data[0] == 0x79:
+            reset_all_controllers()
+        else:
+            print('UNHANDLED CHAN MOD')
+    return bytes_read
+
+def read_pitch_bender(f, channel):
+    print('PITCH BENDER')
+    data = f.read(2)
+    bytes_read = 2
+    return bytes_read
+
+def read_program_change(f, channel):
+    print('PROGRAM CHANGE')
+    data = f.read(1)
+    bytes_read = 1
+    return bytes_read
+
+def read_note_on(f, channel):
+    data = f.read(2)
+    key = hex(data[0])
+    velocity = hex(data[1])
+    print('NOTE ON', key, velocity)
+    return 2
+
+def read_note_off(f, channel):
+    data = f.read(2)
+    key = hex(data[0])
+    velocity = hex(data[1])
+    print('NOTE OFF', key, velocity)
+    return 2
+
+def read_event(f, event):
+    if event == b'\xFF':
+        bytes_read = read_meta(f)
+    elif event == b'\xF0':
+        bytes_read = read_sysex(f)
+    elif b'\x80' <= event <= b'\x8F': # note on
+        channel = ord(event) - ord(b'\x90')
+        bytes_read = read_note_off(f, channel)
+    elif b'\x90' <= event <= b'\x9F': # note on
+        channel = ord(event) - ord(b'\x90')
+        bytes_read = read_note_on(f, channel)
+    elif b'\xB0' <= event <= b'\xBF': # controller change
+        channel = ord(event) - ord(b'\xB0')
+        bytes_read = read_controller_change(f, channel)
+    elif b'\xB0' <= event <= b'\xBF': # controller change
+        channel = ord(event) - ord(b'\xB0')
+        bytes_read = read_controller_change(f, channel)
+    elif b'\xB0' <= event <= b'\xBF': # controller change
+        channel = ord(event) - ord(b'\xB0')
+        bytes_read = read_controller_change(f, channel)
+    elif b'\xC0' <= event <= b'\xCF': # program change
+        channel = ord(event) - ord(b'\xC0')
+        bytes_read = read_program_change(f, channel)
+    elif b'\xE0' <= event <= b'\xEF': # pitch bender
+        channel = ord(event) - ord(b'\xE0')
+        bytes_read = read_pitch_bender(f, channel)
+    else:
+        bytes_read = -1
+    return bytes_read
+ 
 def read_track(f):
     chunk_type = f.read(4)
     chunk_length = read32bit(f)
+    print(chunk_type)
     assert chunk_type == CHUNK_TYPE_TRACK
     print(chunk_length)
     bytes_left = chunk_length
@@ -72,14 +147,22 @@ def read_track(f):
         print('EVENT_TIME', event_time)
         bytes_left -= bytes_read
         event = f.read(1)
-        print('EVENT', hex(ord(event)))
-        if event == b'\xFF':
-            bytes_read = read_meta(f)
-        elif event == b'\xF0':
-            bytes_read = read_sysex(f)
+        if event < b'\x80':
+            # handle running status by moving file pointer back by 1 and set event to the running event
+            f.seek(-1, io.SEEK_CUR)
+            event = running
+            bytes_left += 1
+            print('RUNNING EVENT', hex(ord(event)))
         else:
+            print('EVENT', hex(ord(event)))
+        bytes_read = read_event(f, event)
+        if bytes_read == -1:
+            print('BREAKING BADLY')
             break
+        running = event
         bytes_left -= (1+bytes_read)
+    print('end of track', bytes_left)
+    assert bytes_left == 0
     f.read(bytes_left)
 
 path = 'Uriah_Heep_Lady_In_Black.mid'
@@ -87,4 +170,3 @@ with open(path, 'rb') as f:
     form, tracks, division = read_header(f)
     for _ in range(tracks):
         read_track(f)
-    # do stuff 
